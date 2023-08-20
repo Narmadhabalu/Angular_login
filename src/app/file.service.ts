85b9c68955d80ecc04a2b68a1be8ace89885c7b8
Narmadhabalu <140816564+Narmadhabalu@users.noreply.github.com>
2023-08-20 12:49:24 +0000

    Add files via upload

--- src/app/file.service.ts
+++ src/app/file.service.ts
@@ -6,12 +6,19 @@ import { Injectable } from '@angular/core';
 export class FileService {
 
   constructor() { }
+  email = 'narmadha@27';
+  password = 'nar27';
+  public GetText(){
+    return 'string from service';
+  }
 
   student=[
     {name:'narmadha',gender:'female'},
     {name:'kiruthiga',gender:'female'},
     {name:'rohith',gender:'male'},
-    {name:'harini',gender:'female'}
+    {name:'harini',gender:'female'},
+    {name:'suriya',gender:'male'},
+    {name:'balu',gender:'male'},
   ]
 
   public getStudent(){
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username:string='';
+  email:string='';
   password:string='';
 
   constructor(private router:Router){
